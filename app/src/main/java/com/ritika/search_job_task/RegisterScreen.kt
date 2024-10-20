b4cd059b5ddd32a539a8bd13df9c577269b8316d
Ritika <ritikatiwari784@gmail.com>
2024-10-20 19:56:02 +0530

    linked the login screen to register screen TextView using fragments

--- app/src/main/java/com/ritika/search_job_task/RegisterScreen.kt
+++ app/src/main/java/com/ritika/search_job_task/RegisterScreen.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 
 class RegisterScreen : Fragment() {
@@ -20,8 +21,25 @@ class RegisterScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_screen, container, false)
+        val view = inflater.inflate(R.layout.fragment_register_screen, container, false)
+
+
+        view.findViewById<TextView>(R.id.register_create_account).setOnClickListener {
+            // Call the function to navigate to LoginScreen
+            val loginFragment = LoginScreen()
+
+            // Use parentFragmentManager instead of supportFragmentManager
+            val transaction = parentFragmentManager.beginTransaction()
+            parentFragmentManager.popBackStack()  // This removes previous fragment from the stack
+            transaction.replace(R.id.main_activity, loginFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+        return view
     }
+
+
 
     companion object {
 
